90a2b3963172af5f6e6140ac17a004106626dd6f
brkygngr <berkaygungor.1996@gmail.com>
2024-07-27 22:47:39 +0300

    test(exchangeConversionController): returns bad request when source currency is null or blank

--- src/main/java/com/brkygngr/foreign_exchange/exchange_conversion/dto/ExchangeConversionRequest.java
+++ src/main/java/com/brkygngr/foreign_exchange/exchange_conversion/dto/ExchangeConversionRequest.java
@@ -1,5 +1,6 @@
 package com.brkygngr.foreign_exchange.exchange_conversion.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 public record ExchangeConversionRequest(@NotNull(message = "Amount is required!")
                                         @Positive(message = "Amount must be positive!")
                                         BigDecimal amount,
+                                        @NotBlank(message = "Source currency is required!")
                                         String sourceCurrency,
                                         String targetCurrency) {
 
--- src/test/java/com/brkygngr/foreign_exchange/exchange_conversion/controller/ExchangeConversionControllerTest.java
+++ src/test/java/com/brkygngr/foreign_exchange/exchange_conversion/controller/ExchangeConversionControllerTest.java
@@ -1,15 +1,11 @@
 package com.brkygngr.foreign_exchange.exchange_conversion.controller;
 
 import com.brkygngr.foreign_exchange.exchange_conversion.dto.ExchangeConversionRequest;
-import com.brkygngr.foreign_exchange.exchange_conversion.service.ExchangeConversionService;
-import com.brkygngr.foreign_exchange.exchange_rate.controller.ExchangeRateController;
-import com.brkygngr.foreign_exchange.exchange_rate.service.ExchangeRateService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,7 +16,6 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mockStatic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -96,6 +91,50 @@ class ExchangeConversionControllerTest {
                    .andExpect(status().isBadRequest())
                    .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").value(timestamp))
                    .andExpect(MockMvcResultMatchers.jsonPath("$.errors").value("Amount must be positive!"));
+        }
+    }
+
+    @Test
+    void getConversion_whenSourceCurrencyIsNull_returnsBadRequestErrorResponse() throws Exception {
+        final String timestamp = "2024-01-01T00:00:00Z";
+
+        final Instant instant = Instant.now(Clock.fixed(Instant.parse(timestamp), ZoneId.of("UTC")));
+
+        try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(instant);
+
+            final ExchangeConversionRequest request = new ExchangeConversionRequest(BigDecimal.ONE, null, "USD");
+
+            mockMvc.perform(MockMvcRequestBuilders
+                                    .post("/api/v1/conversion")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(request))
+                                    .accept(MediaType.APPLICATION_JSON))
+                   .andExpect(status().isBadRequest())
+                   .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").value(timestamp))
+                   .andExpect(MockMvcResultMatchers.jsonPath("$.errors").value("Source currency is required!"));
+        }
+    }
+
+    @Test
+    void getConversion_whenSourceCurrencyIsEmptyString_returnsBadRequestErrorResponse() throws Exception {
+        final String timestamp = "2024-01-01T00:00:00Z";
+
+        final Instant instant = Instant.now(Clock.fixed(Instant.parse(timestamp), ZoneId.of("UTC")));
+
+        try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(instant);
+
+            final ExchangeConversionRequest request = new ExchangeConversionRequest(BigDecimal.ONE, "", "USD");
+
+            mockMvc.perform(MockMvcRequestBuilders
+                                    .post("/api/v1/conversion")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(request))
+                                    .accept(MediaType.APPLICATION_JSON))
+                   .andExpect(status().isBadRequest())
+                   .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").value(timestamp))
+                   .andExpect(MockMvcResultMatchers.jsonPath("$.errors").value("Source currency is required!"));
         }
     }
 }

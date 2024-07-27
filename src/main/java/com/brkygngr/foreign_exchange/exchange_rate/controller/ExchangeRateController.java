2c92bc8bc4400b9742d7ec64d5da18dc7621b437
brkygngr <berkaygungor.1996@gmail.com>
2024-07-27 17:54:12 +0300

    docs: reflect error responses in swagger documentation

--- src/main/java/com/brkygngr/foreign_exchange/exchange_rate/controller/ExchangeRateController.java
+++ src/main/java/com/brkygngr/foreign_exchange/exchange_rate/controller/ExchangeRateController.java
@@ -1,9 +1,14 @@
 package com.brkygngr.foreign_exchange.exchange_rate.controller;
 
+import com.brkygngr.foreign_exchange.exception.ErrorResponse;
 import com.brkygngr.foreign_exchange.exchange_rate.dto.ExchangeRate;
 import com.brkygngr.foreign_exchange.exchange_rate.dto.ExchangeRateQuery;
 import com.brkygngr.foreign_exchange.exchange_rate.service.ExchangeRateService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -21,6 +26,20 @@ public class ExchangeRateController {
 
     @GetMapping("/exchange-rate")
     @Operation(summary = "Returns exchange rate from source to target currency")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response",
+                         content = @Content(
+                                 mediaType = "application/json",
+                                 schema = @Schema(implementation = ExchangeRate.class))),
+            @ApiResponse(responseCode = "400", description = "Validation errors",
+                         content = @Content(
+                                 mediaType = "application/json",
+                                 schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "422", description = "Currency invalid errors",
+                         content = @Content(
+                                 mediaType = "application/json",
+                                 schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public ResponseEntity<ExchangeRate> getExchangeRateBetween(@ParameterObject
                                                                @Valid
                                                                ExchangeRateQuery exchangeRateQuery) {

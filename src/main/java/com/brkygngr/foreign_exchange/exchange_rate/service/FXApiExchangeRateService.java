6491df87f924900188654051d957e4a3f6f0e6ea
brkygngr <berkaygungor.1996@gmail.com>
2024-07-28 22:42:18 +0300

    feat(fxExchangeRateService): cache exchange requests for a day

--- src/main/java/com/brkygngr/foreign_exchange/exchange_rate/service/FXApiExchangeRateService.java
+++ src/main/java/com/brkygngr/foreign_exchange/exchange_rate/service/FXApiExchangeRateService.java
@@ -1,20 +1,16 @@
 package com.brkygngr.foreign_exchange.exchange_rate.service;
 
 import com.brkygngr.foreign_exchange.exception.ResponseNullException;
-import com.brkygngr.foreign_exchange.exception.InvalidCurrencyException;
 import com.brkygngr.foreign_exchange.exchange_rate.dto.ExchangeRate;
 import com.brkygngr.foreign_exchange.fx_api.FXApiAccessor;
 import com.brkygngr.foreign_exchange.fx_api.dto.FXApiCurrencyRate;
 import com.brkygngr.foreign_exchange.fx_api.dto.FXApiExchangeRateResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +19,7 @@ public class FXApiExchangeRateService implements ExchangeRateService {
 
     private final FXApiAccessor fxApiAccessor;
 
+    @Cacheable(value = "exchange_rate", key = "#sourceCurrency + '_to_' + #targetCurrency")
     public ExchangeRate getLatestExchangeRateBetween(final String sourceCurrency, final String targetCurrency) {
         FXApiExchangeRateResponse fxApiExchangeRateResponse = fxApiAccessor
                 .getLatestExchangeRateBetween(sourceCurrency,
@@ -38,5 +35,11 @@ public class FXApiExchangeRateService implements ExchangeRateService {
         FXApiCurrencyRate currencyRate = fxApiExchangeRateResponse.data().get(targetCurrency);
 
         return currencyRate.toExchangeRate(sourceCurrency);
+    }
+
+    @CacheEvict(value = "exchange_rate", allEntries = true)
+    @Scheduled(cron = "${app.cache.exchange-rate.ttl}")
+    public void emptyExchangeRateCacheEveryday() {
+        log.info("Clearing exchange rate cache");
     }
 }
